5a2101cbd56ff8f6c66671b043d09e26907ea481
Aplodise <ronmacdonald228@gmail.com>
2024-06-24 10:56:25 +0200

    Added GetBusinessCustomerByKRS method

--- APBDFinalProject/APBDFinalProject/Repositories/Abstractions/IBusinessCustomerRepository.cs
+++ APBDFinalProject/APBDFinalProject/Repositories/Abstractions/IBusinessCustomerRepository.cs
@@ -5,5 +5,6 @@ namespace APBDFinalProject.Repositories;
 public interface IBusinessCustomerRepository
 {
     Task<int> AddCustomer(BusinessCustomer businessCustomer, CancellationToken cancellationToken);
-    Task<int> UpdateCustomer(int id, BusinessCustomer businessCustomer, CancellationToken cancellationToken);
+    Task<int> UpdateCustomer(int krs, BusinessCustomer businessCustomer, CancellationToken cancellationToken);
+    Task<BusinessCustomer?> GetBusinessCustomerByKRS(int krs, CancellationToken cancellationToken);
 }
--- APBDFinalProject/APBDFinalProject/Repositories/BusinessCustomerRepository.cs
+++ APBDFinalProject/APBDFinalProject/Repositories/BusinessCustomerRepository.cs
@@ -20,14 +20,19 @@ public class BusinessCustomerRepository : IBusinessCustomerRepository
         return businessCustomer.Id;
     }
 
-    public async Task<int> UpdateCustomer(int id, BusinessCustomer businessCustomer, CancellationToken cancellationToken)
+    public async Task<int> UpdateCustomer(int krs, BusinessCustomer businessCustomer, CancellationToken cancellationToken)
     {
-        var affectedRows = await _context.BusinessCustomers.Where(e => e.Id == id)
+        var affectedRows = await _context.BusinessCustomers.Where(e => e.KRS == krs)
             .ExecuteUpdateAsync(updates =>
                 updates.SetProperty(customer => customer.BusinessName, businessCustomer.BusinessName)
                     .SetProperty(customer => customer.Email, businessCustomer.Email)
                     .SetProperty(customer => customer.Address, businessCustomer.Address)
                     .SetProperty(customer => customer.PhoneNumber, businessCustomer.PhoneNumber), cancellationToken);
         return affectedRows;
+    }
+
+    public async Task<BusinessCustomer?> GetBusinessCustomerByKRS(int krs, CancellationToken cancellationToken)
+    {
+        return await _context.BusinessCustomers.FirstOrDefaultAsync(c => c.KRS == krs, cancellationToken);
     }
 }
