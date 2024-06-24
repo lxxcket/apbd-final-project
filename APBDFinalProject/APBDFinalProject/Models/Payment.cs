c5299e868e14b4d86e03c1e524fc540fe5e8a9b2
Aplodise <ronmacdonald228@gmail.com>
2024-06-24 15:51:30 +0200

    Added Customer field.

--- APBDFinalProject/APBDFinalProject/Models/Payment.cs
+++ APBDFinalProject/APBDFinalProject/Models/Payment.cs
@@ -8,11 +8,16 @@ public class Payment
 {
     [Key]
     public int Id { get; set; }
+    [Required]
     public int IdContract { get; set; }
+    [Required]
+    public int IdCustomer { get; set; }
     [Required]
     public DateTime PaymentDate { get; set; }
     [Required]
     public decimal AmountPaid { get; set; }
     [ForeignKey(nameof(IdContract))]
     public virtual Contract Contract { get; set; }
+    [ForeignKey(nameof(IdCustomer))]
+    public virtual Customer Customer { get; set; }
 }
