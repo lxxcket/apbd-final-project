8b6cfd688202868e217e69ecaf5b5169f106cdbc
Aplodise <ronmacdonald228@gmail.com>
2024-06-25 15:01:22 +0200

    Small improvements

--- APBDFinalProject/APBDFinalProject/Program.cs
+++ APBDFinalProject/APBDFinalProject/Program.cs
@@ -1,5 +1,8 @@
 using System.Text;
 using APBDFinalProject.Contexts;
+using APBDFinalProject.Repositories;
+using APBDFinalProject.Services;
+using APBDFinalProject.Services.Abstractions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -19,11 +22,16 @@ public class Program
         var connectionString = builder.Configuration.GetConnectionString("MyConnectionString");
         builder.Services.AddDbContext<IncomeContext>(option => option.UseSqlServer(connectionString));
         //
-        // builder.Services.AddScoped<IPrescriptionAddingPolicy, PrescriptionAddingPolicy>();
+        builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+        builder.Services.AddScoped<IBusinessCustomerRepository, BusinessCustomerRepository>();
+        builder.Services.AddScoped<IIndividualCustomerRepository, IndividualCustomerRepository>();
+        builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+        builder.Services.AddScoped<IContractRepository, ContractRepository>();
+        builder.Services.AddScoped<IVersionRepository, VersionRepository>();
         //
-        // builder.Services.AddScoped<IPatientRepository, PatientRepository>();
-        // builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-        // builder.Services.AddScoped<IPrescriptionMedicamentRepository, PrescriptionMedicamentRepository>();
+        builder.Services.AddScoped<IBusinessCustomerService, BusinessCustomerService>();
+        builder.Services.AddScoped<IContractService, ContractService>();
+        builder.Services.AddScoped<IIndividualCustomerService, IndividualCustomerService>();
         // builder.Services.AddScoped<IMedicamentRepository, MedicamentRepository>();
         // builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
         //
@@ -33,46 +41,46 @@ public class Program
         // builder.Services.AddScoped<IUserService, UserService>();
         
         
-        builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(opt =>
-        {
-            opt.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,  
-                ValidateAudience = true,
-                ValidateLifetime = true,
-                ClockSkew = TimeSpan.FromMinutes(2),
-                ValidIssuer = "https://localhost:5181", 
-                ValidAudience = "https://localhost:5181",
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secret"]))
-            };
-            opt.Events = new JwtBearerEvents
-            {
-                OnAuthenticationFailed = context =>
-                {
-                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                    {
-                        context.Response.Headers.Add("Token-expired", "true");
-                    }
-                    return Task.CompletedTask;
-                }
-            };
-        }).AddJwtBearer("IgnoreTokenExpirationScheme",opt =>
-        {
-            opt.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,   
-                ValidateAudience = true, 
-                ValidateLifetime = false,
-                ClockSkew = TimeSpan.FromMinutes(2),
-                ValidIssuer = "https://localhost:5181", 
-                ValidAudience = "https://localhost:5181", 
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secret"]))
-            };
-        });
+        // builder.Services.AddAuthentication(options =>
+        // {
+        //     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        // }).AddJwtBearer(opt =>
+        // {
+        //     opt.TokenValidationParameters = new TokenValidationParameters
+        //     {
+        //         ValidateIssuer = true,  
+        //         ValidateAudience = true,
+        //         ValidateLifetime = true,
+        //         ClockSkew = TimeSpan.FromMinutes(2),
+        //         ValidIssuer = "https://localhost:5181", 
+        //         ValidAudience = "https://localhost:5181",
+        //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secret"]))
+        //     };
+        //     opt.Events = new JwtBearerEvents
+        //     {
+        //         OnAuthenticationFailed = context =>
+        //         {
+        //             if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+        //             {
+        //                 context.Response.Headers.Add("Token-expired", "true");
+        //             }
+        //             return Task.CompletedTask;
+        //         }
+        //     };
+        // }).AddJwtBearer("IgnoreTokenExpirationScheme",opt =>
+        // {
+        //     opt.TokenValidationParameters = new TokenValidationParameters
+        //     {
+        //         ValidateIssuer = true,   
+        //         ValidateAudience = true, 
+        //         ValidateLifetime = false,
+        //         ClockSkew = TimeSpan.FromMinutes(2),
+        //         ValidIssuer = "https://localhost:5181", 
+        //         ValidAudience = "https://localhost:5181", 
+        //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Secret"]))
+        //     };
+        // });
         
         var app = builder.Build();
 
