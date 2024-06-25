01e052c4f09432430d3f918848b9605edff8b1fa
Aplodise <ronmacdonald228@gmail.com>
2024-06-25 15:00:55 +0200

    IndividualCustomerController implemented

--- APBDFinalProject/APBDFinalProject/Controllers/IndividualCustomerController.cs
+++ APBDFinalProject/APBDFinalProject/Controllers/IndividualCustomerController.cs
@@ -1,3 +1,5 @@
+using APBDFinalProject.Exceptions;
+using APBDFinalProject.RequestModels;
 using APBDFinalProject.Services.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,7 +7,7 @@ namespace APBDFinalProject.Controllers;
 
 
 [ApiController]
-[Route("api/{controller}")]
+[Route("api/[controller]")]
 public class IndividualCustomerController : ControllerBase
 {
     private IIndividualCustomerService _individualCustomerService;
@@ -14,6 +16,54 @@ public class IndividualCustomerController : ControllerBase
     {
         _individualCustomerService = individualCustomerService;
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateIndividualCustomer(IndividualCustomerRequest individualCustomerRequest,
+        CancellationToken cancellationToken)
+    {
+        int id;
+        try
+        {
+            id = await _individualCustomerService.AddCustomer(individualCustomerRequest, cancellationToken);
+        }
+        catch (DomainException e)
+        {
+            return BadRequest(e.Message);
+        }
+
+        return Ok(id);
+    }
     
-    
+    [HttpPut("{pesel:long}")]
+    public async Task<IActionResult> UpdateIndividualCustomer(long pesel,  IndividualCustomerUpdateRequest individualCustomerRequest,
+        CancellationToken cancellationToken)
+    {
+        int id;
+        try
+        {
+            id = await _individualCustomerService.UpdateCustomer(pesel, individualCustomerRequest, cancellationToken);
+        }
+        catch (DomainException e)
+        {
+            return BadRequest(e.Message);
+        }
+
+        return NoContent();
+    }
+    [HttpDelete("{pesel:long}")]
+    public async Task<IActionResult> DeleteIndividualCustomer(long pesel,
+        CancellationToken cancellationToken)
+    {
+        int id;
+        try
+        {
+            await _individualCustomerService.DeleteCustomer(pesel, cancellationToken);
+        }
+        catch (DomainException e)
+        {
+            return BadRequest(e.Message);
+        }
+
+        return NoContent();
+    }
 }
